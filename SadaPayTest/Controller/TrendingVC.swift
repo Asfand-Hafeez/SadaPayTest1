77e730bc56639df7da1fb7788085a392a6b6e42f
Zain-ul-Abideen <aayangill87@gmail.com>
2022-08-02 22:12:58 +0500

    project setup
    project setup
    spm added
    mvvm structure added

---
+++ SadaPayTest/Controller/TrendingVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SadaPayTest
+//
+//  Created by Asfand Hafeez on 02/08/2022.
+//
+
+import UIKit
+
+class TrendingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SadaPayTest/Xib/TrendingCell.swift
@@ -0,0 +1,19 @@
+//
+//  TrendingCell.swift
+//  SadaPayTest
+//
+//  Created by Asfand Hafeez on 02/08/2022.
+//
+
+import UIKit
+
+class TrendingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+}
