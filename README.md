b710d55d0e5a229a33de9e5b9a757bb7622c383d
Zain-ul-Abideen <aayangill87@gmail.com>
2022-08-10 16:35:44 +0500

    .
    .

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SadaPayTest1
  
+ # What I do :
+Design Pattern MVVM
+Shimmer Animation 
+Local Searching 
+JSON Parsing Codable 
+Network Using URL Session 
+Lottie Animation 
+
+ 
--- SadaPayTest/Constant/Constant.swift
+++ SadaPayTest/Constant/Constant.swift
@@ -9,8 +9,7 @@ import Foundation
 
 
 typealias Completion<T> = (Result<T,Error>) -> Void
-let BASE_URL = "https://api.github.com/search/repositories?q=language=+sort:st"
-
+let BASE_URL = "https://api.github.com/search/repositories?q=language=+sort:stars"
 
 
 
